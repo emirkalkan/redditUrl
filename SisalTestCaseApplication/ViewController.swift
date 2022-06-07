f0cec973f32acf638fe9206063e48fcb63b295f3
Emir <emir.zeus@hotmail.com>
2022-06-07 17:19:01 +0300

    Initial Commit

---
+++ SisalTestCaseApplication/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SisalTestCaseApplication
+//
+//  Created by Emir Kalkan on 7.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
